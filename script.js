5984f0813610bc9a56d4cfca634a1a4d298ff9a9
dashtya7mad-star <dashtya7mad@gmail.com>
2026-08-27 00:45:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const BOT_TOKEN = '8422907042:AAE4hkxpTZtfJh5wDlJEI9NeLLjR3jt7XNQ';
 const CHAT_ID = '5015426926';
-const GROUP_LINK = 'https://facebook.com/reel/1337999274989922/?fs=e&fs=e';
+const GROUP_LINK = 'https://www.facebook.com/reel/1363942655583132/?fs=e&fs=e';
