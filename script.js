3ca710bb1e14aa22953cc6a31daf1c0085c23faa
dashtya7mad-star <dashtya7mad@gmail.com>
2026-08-27 23:49:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 const BOT_TOKEN = '8422907042:AAE4hkxpTZtfJh5wDlJEI9NeLLjR3jt7XNQ';
 const CHAT_ID = '5015426926';
-const GROUP_LINK = 'https://www.facebook.com/reel/1363942655583132/?fs=e&fs=e';
+function openFacebook() {
+    const facebookApp = 'fb://facewebmodal/f?href=' + encodeURIComponent(
+        'https://www.facebook.com/reel/1363942655583132/'
+    );
+
+    window.location.href = facebookApp;
+
+    setTimeout(() => {
+        window.location.href =
+            'https://www.facebook.com/reel/1363942655583132/';
+    }, 1500);
+}
